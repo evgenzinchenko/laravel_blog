9f731f96688f95ce5bb1227b692b972d7c560cc7
evgen <evgen159@ukr.net>
2022-08-11 12:51:37 +0300

    Создание связей сущностей

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    use HasFactory;
+    public function post()
+    {
+    	return $this->hasOne(Post::class);
+    }
+
+    public function author()
+    {
+    	return $this->hasOne(User::class);
+    }
 }
